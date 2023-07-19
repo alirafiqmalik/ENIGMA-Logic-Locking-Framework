461accf00003546faf701abae1b1eafb2394ea60
alirafiqmalik <amalik.bee19seecs@seecs.edu.pk>
2023-07-19 14:48:38 +0500

    modified:   tmp1.py

--- tmp1.py
+++ tmp1.py
@@ -20,41 +20,88 @@ import time
 # '
 # """
 
-# pathin="input_files/ASSURE_LOCKED/design1/design1_netlist.v"
+top="fsm_0_obf"
+pathin="input_files/ASSURE_LOCKED/design1/design1_netlist.v"
+
+# top="fsm"
 # pathin="input_files/ASSURE_LOCKED/design1/oracle1_netlist.v"
 
 # utils.clean_dir("./tmp")
 
-# obj=AST(file_path=pathin,rw="w",flag="v",top=top,filename=f"{top}org",sub_modules="input_files/ASSURE_LOCKED/modulefiles.v")#Run to Read in Verilog Design
+obj=AST(file_path=pathin,rw="w",flag="v",top=top,filename=f"{top}org",vlibpath="input_files/ASSURE_LOCKED/modulefiles.v",synth=False)#Run to Read in Verilog Design
 
 
 
-top="fsm_0_obf"
-locked = AST(file_path=f"./output_files/{top}org.json",rw='r',filename=f"{top}locked_test") #Run to read in AST Format
-top="fsm"
-unlocked = AST(file_path=f"./output_files/{top}org.json",rw='r',filename=f"{top}locked_test") #Run to read in AST Format
+# top="fsm_0_obf"
+# locked = AST(file_path=f"./output_files/{top}org.json",rw='r',filename=f"{top}locked_test") #Run to read in AST Format
+# top="fsm"
+# unlocked = AST(file_path=f"./output_files/{top}org.json",rw='r',filename=f"{top}locked_test") #Run to read in AST Format
 
 
-diff=utils.get_diference(locked.top_module.io["outputs"],unlocked.top_module.io["outputs"])
-com=utils.find_common_elements(locked.top_module.io["outputs"],unlocked.top_module.io["outputs"])
+# outn=locked.top_module.io["outputs"]
 
-keys=[]
-inpb=[]
-inpa=list(unlocked.top_module.io["inputs"].keys())
+# import sys
 
-for i in locked.top_module.io["inputs"]:
-  if("locking" not in i):
-    inpb.append(i)
-  else:
-    keys.append(i)
+# #1000
+# print(sys.getrecursionlimit())
+
+# # set recursion limit to 2000
+# sys.setrecursionlimit(200000)
 
 
-print(inpa)
-print()
-print(inpb)
-print()
-print()
+# done=[]
+# keys=[]
+# def return_pred(circuitgraph,node):
+#   global done
+#   global keys
+#   if("module#" in node):
+#     return
+  
+#   if(node in done):
+#       return
+#   else:
+#       done.append(node)
+#   pred=list(circuitgraph.predecessors(node))
+#   if(pred==[]):
+#     return
+
+#   # print(pred)
+#   for i in pred:
+#     if("locking" in i):
+#        keys.append(i)
+#     return_pred(circuitgraph,i)
+    
+
+# locked.top_module.gen_org_verilog()
+# print(locked.top_module.gate_level_verilog)
 
 
-print(list(unlocked.top_module.io["outputs"].keys()))
-print(list(locked.top_module.io["outputs"].keys()))
+# for i in outn:
+#   return_pred(locked.top_module.circuitgraph,i)
+#   print(len(keys))
+#   # break
+
+
+# diff=utils.get_diference(locked.top_module.io["outputs"],unlocked.top_module.io["outputs"])
+# com=utils.find_common_elements(locked.top_module.io["outputs"],unlocked.top_module.io["outputs"])
+
+# keys=[]
+# inpb=[]
+# inpa=list(unlocked.top_module.io["inputs"].keys())
+
+# for i in locked.top_module.io["inputs"]:
+#   if("locking" not in i):
+#     inpb.append(i)
+#   else:
+#     keys.append(i)
+
+
+# print(inpa)
+# print()
+# print(inpb)
+# print()
+# print()
+
+
+# print(list(unlocked.top_module.io["outputs"].keys()))
+# print(list(locked.top_module.io["outputs"].keys()))
