de0bcf5fefbba5e6b8d779b0b6ffb8a004061346
alirafiqmalik <amalik.bee19seecs@seecs.edu.pk>
2023-08-08 20:40:11 +0500

    made improvements to extract_gates_b

--- src/Parser/bench_parser.py
+++ src/Parser/bench_parser.py
@@ -8,20 +8,21 @@ bench_gates=['DFF','BUF','NOT', 'AND', 'OR', 'NAND', 'NOR','XOR','XNOR']
 ####################################################################################################################################
 def extract_gates_b(bench):
     tmp={i:[] for i in bench_gates}
-    matches = re.findall(r"(\w+)\s*=\s*(\w+)\((.*?)\)", bench, re.MULTILINE)
-    output_list = [(match[0], match[1], *re.findall(r"G\d+", match[2])) for match in matches]
+    matches = re.findall(r"(.+)\s*=\s*(.+)\((.*?)\)", bench, re.MULTILINE)
+    output_list = [(match[0], match[1], *match[2].split(",")) for match in matches]
 
-    tmp={i:[] for i in bench_gates}
     for i in output_list:
         if i[1].upper()=='NOT' or i[1].upper()=='BUF' or i[1].upper()=='DFF':
-            tmp[i[1].upper()].append((i[0],i[2]))
+            tmp[i[1].strip().upper()].append((i[0].strip(),i[2].strip()))
             pass
         elif i[1].upper() in bench_gates:
-            tmp[i[1].upper()].append((i[0],i[2:]))
+            tmpl=[i[0].strip()]+[k.strip() for k in i[2:]]
+            tmp[i[1].strip().upper()].append(tmpl)
+        else:
+            raise Exception("Something is wrong here")
     
     gate_count = {i: len(tmp[i]) for i in tmp}
-    
-    return tmp,gate_count
+    return {key:val for key,val in tmp.items() if(val)},gate_count
 
 def extract_io_b(bench,mode="input"):
     tmp=re.findall(mode.upper()+r"\((.*)\)",bench)
