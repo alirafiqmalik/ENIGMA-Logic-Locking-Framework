a3e0928c7b50be96006ce2fb2c275db08218aa77
alirafiqmalik <amalik.bee19seecs@seecs.edu.pk>
2023-08-12 14:15:41 +0500

    Added Classes: Analyze_Netlist, SATobj, cone_ckt

--- src/Netlist/netlist.py
+++ src/Netlist/netlist.py
@@ -1,6 +1,10 @@
 # Deprecated
 import networkx as nx
 import re
+from node import Node
+import attacks.satattack.circuit as circuit
+import src.utils as utils
+
 
 class Netlist():
   def __init__(self,netlist:str)->None:#mode:str="graph"
@@ -118,3 +122,98 @@ class Netlist():
     print("Writing BENCH File to Location: ",outpath)
     with open(outpath, 'w') as f:
       f.write(graphtonetlist)
+
+
+
+class cone_ckt:
+  def __init__(self,outputs,primary_inputs,key_inputs,nodes,depth) -> None:
+    self.primary_inputs=primary_inputs
+    self.key_inputs=key_inputs
+    self.nodes=nodes
+    self.outputs=outputs
+    self.depth=depth
+
+class SATobj:
+  def __init__(self,oracle_ckt:cone_ckt,locked_ckt:cone_ckt) -> None:
+    self.oracle_ckt_cone=oracle_ckt
+    self.locked_ckt_cone=locked_ckt
+    self.keyinputs=self.locked_ckt_cone.key_inputs
+    self.prim_inputs=self.locked_ckt_cone.primary_inputs
+    
+    self.oracle_ckt=circuit.Circuit.from_nodes(self.oracle_ckt_cone.nodes, self.oracle_ckt_cone.outputs)
+    self.locked_ckt=(self.locked_ckt_cone.nodes, self.locked_ckt_cone.outputs)
+
+
+class Analyze_Netlist:
+  def __init__(self,locked_module_obj=None,unlocked_module_obj=None) -> None:
+    self.locked=locked_module_obj
+    self.unlocked=unlocked_module_obj
+    self.attack_outputs={}
+
+
+    if(self.locked!=None and self.unlocked!=None):
+      self.common_outputs=utils.get_common_elements(self.locked.io["outputs"],self.unlocked.io["outputs"])
+      self.diff_outputs=utils.get_difference(self.locked.io["outputs"],self.unlocked.io["inputs"])
+      self.gen_cone_data()
+
+  def gen_cone_data(self):
+    def traverse_graph_r(obj,out_node):
+      if(obj.circuitgraph.nodes[out_node]["type"]=="input"):
+          if("key" in out_node and out_node not in self.keyinputs):
+            self.keyinputs.append(out_node)
+            self.nodes[out_node]=Node(out_node,[],"Key Input")
+          elif(out_node not in self.primary_inp):
+              self.primary_inp.append(out_node)
+              self.nodes[out_node]=Node(out_node,[],"Primary Input")
+          return 
+      elif(obj.circuitgraph.nodes[out_node]["type"]=="output"):
+          if(out_node not in self.out):
+            self.out.append(out_node)
+      elif(out_node in self.nodes):
+        return
+      
+      pred=obj.circuitgraph.predecessors(out_node)
+      self.depth+=1
+      for i in pred:
+        if(obj.circuitgraph.nodes[i]["type"]=="gate"): 
+          self.nodes[out_node]=Node(out_node,list(obj.circuitgraph.predecessors(i)),utils.det_logic(obj.circuitgraph.nodes[i]["logic"],obj.gates))
+        traverse_graph_r(obj,i) 
+    
+    
+    
+    self.common_outputs.sort(key=lambda s: int(''.join(filter(str.isdigit,s)) or 0))
+
+    for i in self.common_outputs:
+      pred1=list(self.unlocked.circuitgraph.predecessors(i))
+      pred2=list(self.locked.circuitgraph.predecessors(i))
+      if not (pred2 or pred1):
+        print(pred1,pred2)
+        continue
+
+      self.primary_inp=[]
+      self.keyinputs=[]
+      self.out=[]
+      self.nodes={}
+      self.depth=0
+      traverse_graph_r(self.unlocked,i)
+      if(i not in self.out):
+        self.out.append(i)
+      self.out.sort(key=lambda s: int(''.join(filter(str.isdigit,s)) or 0))
+      oracle_ckt=cone_ckt(self.out,self.primary_inp,self.keyinputs,self.nodes,self.depth)
+      
+
+      self.primary_inp=[]
+      self.keyinputs=[]
+      self.out=[]
+      self.nodes={}
+      self.depth=0
+      traverse_graph_r(self.locked,i)
+      if(i not in self.out):
+        self.out.append(i)
+      self.out.sort(key=lambda s: int(''.join(filter(str.isdigit,s)) or 0))
+      locked_ckt=cone_ckt(self.out,self.primary_inp,self.keyinputs,self.nodes,self.depth)
+      
+      self.attack_outputs[i]=SATobj(oracle_ckt,locked_ckt)
+      
+      if(oracle_ckt.outputs==locked_ckt.outputs and locked_ckt.outputs==self.common_outputs):
+        break
