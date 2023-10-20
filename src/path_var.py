2d0c77e8bae03fe4cba599176f0864539265eb79
alirafiqmalik <alirafiqmalik@gmail.com>
2023-10-20 14:46:19 -0400

    On PC: 	deleted:    attacks 	modified:   src/path_var.py

--- src/path_var.py
+++ src/path_var.py
@@ -1,6 +1,5 @@
 
 
 # if yosys is in build dir, give path to yosys bin file e.g 
-yosys_path="~/FYP/linux/yosys/build/yosys"
+yosys_path="~../yosys/yosys"
 # if yosys is installed using apt, write yosys
-# yosys_path="yosys" 
