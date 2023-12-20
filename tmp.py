6e6efd16cb51e2007004eee2a34f02d485943584
Ali Hamza Malik <88540402+alirafiqmalik@users.noreply.github.com>
2023-12-20 18:00:39 -0400

    updated temp files

--- tmp.py
+++ tmp.py
@@ -1,43 +1,46 @@
 
-import re
-import time
 
-# Test patterns
-patterns = [
-    r"^\d+$",                  # Matches one or more digits
-    r"^[A-Za-z]+$",            # Matches one or more alphabetic characters
-    r"^[A-Za-z0-9]+$",         # Matches one or more alphanumeric characters
-    r"^\w+@\w+\.\w+$"          # Matches a basic email address pattern
-]
+# locked = AST(file_path=f"./output_files/{top}org.json",rw='r',filename=f"{top}locked_test") #Run to read in AST Format
 
-# Test strings
-strings = [
-    "1234567890",
-    "abcdefg",
-    "abc123",
-    "john.doe@example.com"
-]
+# import re
+# import time
 
-# Regex libraries to test
-libraries = {
-    "PCRE": re,
-    "RE2": re,
-    "Boost.Regex": re,
-    "Oniguruma": re,
-}
+# # Test patterns
+# patterns = [
+#     r"^\d+$",                  # Matches one or more digits
+#     r"^[A-Za-z]+$",            # Matches one or more alphabetic characters
+#     r"^[A-Za-z0-9]+$",         # Matches one or more alphanumeric characters
+#     r"^\w+@\w+\.\w+$"          # Matches a basic email address pattern
+# ]
 
-# Perform tests
-for pattern in patterns:
-    print(f"Pattern: {pattern}")
-    for library, regex_module in libraries.items():
-        total_time = 0
-        for string in strings:
-            start_time = time.time()
-            regex_module.match(pattern, string)
-            end_time = time.time()
-            elapsed_time = end_time - start_time
-            total_time += elapsed_time
+# # Test strings
+# strings = [
+#     "1234567890",
+#     "abcdefg",
+#     "abc123",
+#     "john.doe@example.com"
+# ]
 
-        avg_time = total_time / len(strings)
-        print(f"{library}: Average Time: {avg_time:.6f} seconds")
-    print()
+# # Regex libraries to test
+# libraries = {
+#     "PCRE": re,
+#     "RE2": re,
+#     "Boost.Regex": re,
+#     "Oniguruma": re,
+# }
+
+# # Perform tests
+# for pattern in patterns:
+#     print(f"Pattern: {pattern}")
+#     for library, regex_module in libraries.items():
+#         total_time = 0
+#         for string in strings:
+#             start_time = time.time()
+#             regex_module.match(pattern, string)
+#             end_time = time.time()
+#             elapsed_time = end_time - start_time
+#             total_time += elapsed_time
+
+#         avg_time = total_time / len(strings)
+#         print(f"{library}: Average Time: {avg_time:.6f} seconds")
+#     print()
